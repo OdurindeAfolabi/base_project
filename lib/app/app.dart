7a39a29e0978a369c231bec1b8767f5ef5265a72
coinvalidator <afod123@gmail.com>
2022-04-11 16:46:51 +0100

    Second commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatefulWidget {
+  MyApp._internal(); //private named constructor
+
+  static final MyApp instance=MyApp._internal(); //single instance --singleton
+  factory MyApp()=> instance; //factory for the class instance
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
